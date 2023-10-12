4f2d6d46d75298adfac8152bb0b4019c3ff888be
Marco Marchi <antoniomarchi2006@gmail.com>
2023-10-12 17:12:43 -0300

    listagem
    primeiro envio sobre a listagem

---
+++ amarelinho/src/listagem/CriarProduto.java
@@ -0,0 +1,25 @@
+package listagem;
+
+public class CriarProduto {
+    public static void main(String[] args) {
+        // Crie instâncias dos produtos
+        ItensTransportados celular = new ItensTransportados("Celular", 0.7);
+        ItensTransportados geladeira = new ItensTransportados("Geladeira", 50);
+        ItensTransportados airFryer = new ItensTransportados("Air Fryer", 3.5);
+        ItensTransportados cadeira = new ItensTransportados("Cadeira", 5.0);
+        ItensTransportados luminaria = new ItensTransportados("Luminária", 0.8);
+        ItensTransportados lavadoraRoupa = new ItensTransportados("Lavadora de Roupa", 15);
+        ItensTransportados playstation5 = new ItensTransportados("PlayStation 5", 3.9);
+        ItensTransportados nintendoSwitch = new ItensTransportados("Nintendo Switch", 0.3);
+
+        //Itens foram criados acima ^
+        //Podem ser usados em outros arquivos agora.
+
+        //Teste dos produtos:
+      /*  System.out.println("Teste Produtos (OBJETOS)");
+        System.out.println("Nome: " + celular.getNome() + ", Peso: " + celular.getPeso() + " kg");
+        System.out.println("Nome: " + geladeira.getNome() + ", Peso: " + geladeira.getPeso() + " kg");
+        System.out.println("Nome: " + airFryer.getNome() + ", Peso: " + airFryer.getPeso() + " kg");
+    }*/
+    }
+}
---
+++ amarelinho/src/listagem/ItensTransportados.java
@@ -0,0 +1,21 @@
+package listagem;
+
+public class ItensTransportados {
+        private String nome;
+        private double peso;
+
+        public ItensTransportados(String nome, double peso) {
+            this.nome = nome;
+            this.peso = peso;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public double getPeso() {
+            return peso;
+        }
+    }
+
+
