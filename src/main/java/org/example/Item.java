aba47ec14a416e2d622c4748fe64cb839c83e493
Bianca Gomes <gomes18bianca@gmail.com>
2023-10-13 11:48:50 -0300

    Apenas para teste, sera excluido futuramente

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//Criado para conseguir fazer e testar os dados estatisticos, baseado no cenario 1
+public class Item {
+    TipoItem tipo;
+    int quantidade;
+
+    public Item (TipoItem tipo, int quantidade) {
+       this.tipo = tipo;
+       this.quantidade = quantidade;
+    }
+
+}
+
