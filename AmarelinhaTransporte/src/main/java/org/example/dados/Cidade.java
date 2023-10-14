460167a63a17ae9e2a328c763ae02df1b2201459
Djênifer Gurskas <gurskasdjenifer@gmail.com>
2023-10-14 16:35:55 -0300

    feat
    tentativas de introduzir métodos

--- AmarelinhaTransporte/src/main/java/org/example/dados/Cidade.java
+++ AmarelinhaTransporte/src/main/java/org/example/dados/Cidade.java
@@ -2,29 +2,15 @@ package org.example.dados;
 
 public class Cidade {
      String nome;
-     int[] distancias;
 
-     public Cidade(String nome, int[] distancias) {
+     public Cidade(String nome) {
           this.nome = nome;
-          this.distancias = distancias;
      }
 
      public String getNome() {
           return nome;
      }
-/*
-     public int getDistancias(Cidade outrasCidade) {
-          int indice = getIndex(outrasCidade);
-          return distancias[indice];
-     }
 
-     private int getIndex(Cidade cidade) {
-          for (int i = 0; i < distancias.length; i++) {
-               if (cidades[i].equals(cidade)) {
-                    return i;
-               }
-          }
 
-          return -1; // retorna -1 caso a cidade não for encontrada
-     }*/
+
 }
--- AmarelinhaTransporte/src/main/java/org/example/dados/Produto.java
+++ AmarelinhaTransporte/src/main/java/org/example/dados/Produto.java
@@ -10,6 +10,7 @@ public class Produto {
      }
 
      public String getNome(){
+
           return nome;
      }
 
