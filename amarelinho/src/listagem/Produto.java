da39fe45c8a5992437e425d1b66ccf9ddd581721
Marco Marchi <antoniomarchi2006@gmail.com>
2023-10-14 21:52:19 -0300

    comentários
    ...

--- amarelinho/src/listagem/Produto.java
+++ amarelinho/src/listagem/Produto.java
@@ -1,8 +1,10 @@
 package listagem;
+//Referencia Java
 public class Produto {
     private String nome;
     private double peso;
 
+    //metodo
     public Produto(String nome, double peso) {
         this.nome = nome;
         this.peso = peso;
